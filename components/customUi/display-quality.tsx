895a17d9ecde757be601c9cf714eb1cb303c84c4
venilsutariya <venilsutariya7@gmail.com>
2024-02-09 17:47:18 +0530

    add code

--- components/customUi/display-quality.tsx
+++ components/customUi/display-quality.tsx
@@ -85,135 +85,138 @@ const DisplayQuality = ({
                         <TableHead></TableHead>
                     </TableRow>
                 </TableHeader>
-                {
-                    quality.map((quality: any, index) => (
-                        <TableRow key={index}>
-                            <TableCell>
-                                <Dialog>
-                                    <DialogTrigger>
-                                        <img src={quality.photo} className="h-10 w-10 object-cover" alt="photo" />
-                                    </DialogTrigger>
-                                    <DialogContent>
-                                        <img src={quality.photo} className="h-full w-full object-cover" alt="photo" />
-                                    </DialogContent>
-                                </Dialog>
-                            </TableCell>
-                            <TableCell className="ps-4">
-                                {quality.qualityName}
-                            </TableCell>
-                            <TableCell className="ps-4">
-                                <Drawer>
-                                    <DrawerTrigger asChild>
-                                        <Button variant={"outline"} size={"sm"}>
-                                            Details
-                                        </Button>
-                                    </DrawerTrigger>
-                                    <DrawerContent className="mb-4">
-                                        <DrawerHeader>
-                                            <DrawerTitle>Costing</DrawerTitle>
-                                        </DrawerHeader>
-                                        <div className="px-3 pb-3">
-                                            <Table className=" text-center">
-                                                <TableRow>
-                                                    <TableCell>Yarn Cost:</TableCell>
-                                                    <TableCell>{quality.yarnCost} Rs.</TableCell>
-                                                </TableRow>
-                                                <TableRow>
-                                                    <TableCell>Warping Cost:</TableCell>
-                                                    <TableCell>{quality.warpingCost} Rs.</TableCell>
-                                                </TableRow>
-                                                <TableRow>
-                                                    <TableCell>Extra Cost:</TableCell>
-                                                    <TableCell>{quality.extraCost} Rs.</TableCell>
-                                                </TableRow>
-                                                <TableRow>
-                                                    <TableCell>Job Cost:</TableCell>
-                                                    <TableCell>{quality.jobCost} Rs.</TableCell>
-                                                </TableRow>
-                                                <TableRow>
-                                                    <TableCell>Add Profit:</TableCell>
-                                                    <TableCell>{quality.addProfit} Rs.</TableCell>
-                                                </TableRow>
-                                                <TableRow>
-                                                    <TableCell>Selling Price:</TableCell>
-                                                    <TableCell>{quality.sellingPrice} Rs.</TableCell>
-                                                </TableRow>
-                                            </Table>
-                                        </div>
-                                        <ScrollArea className="h-[400px] overflow-auto bg-zinc-100">
-                                            <DrawerHeader>
-                                                <DrawerTitle>Construction</DrawerTitle>
-                                            </DrawerHeader>
-                                            <Table className=" text-center">
-                                                <TableRow>
-                                                    <TableCell>Yarn:</TableCell>
-                                                    <TableCell>{quality.yarnType}</TableCell>
-                                                </TableRow>
-                                                <TableRow>
-                                                    <TableCell>Yarn Type:</TableCell>
-                                                    <TableCell>{quality.whichYarn}</TableCell>
-                                                </TableRow>
-                                            </Table>
-                                            <DrawerHeader className="p-2">
-                                                <DrawerTitle className=" font-normal text-md">Yarn Detail</DrawerTitle>
-                                            </DrawerHeader>
-                                            <ScrollArea className="h-[150px] text-start rounded-md border p-2">
-                                                {quality.yarnDetail}
-                                            </ScrollArea>
-                                            <DrawerHeader className="p-2">
-                                                <DrawerTitle className=" font-normal text-md">Yarn Pattern</DrawerTitle>
-                                            </DrawerHeader>
-                                            <ScrollArea className="h-[150px] text-start rounded-md border p-2">
-                                                {quality.yarnPattern}
-                                            </ScrollArea>
-                                            <DrawerHeader className="p-2">
-                                                <DrawerTitle className=" font-normal text-md">Reed</DrawerTitle>
-                                            </DrawerHeader>
-                                            <ScrollArea className="h-[100px] text-start rounded-md border p-2">
-                                                {quality.reed}
-                                            </ScrollArea>
-                                            <DrawerHeader className="p-2">
-                                                <DrawerTitle className=" font-normal text-md">Reed Pattern</DrawerTitle>
-                                            </DrawerHeader>
-                                            <ScrollArea className="h-[100px] text-start rounded-md border p-2">
-                                                {quality.reedPattern}
-                                            </ScrollArea>
-                                            <DrawerHeader className="p-2">
-                                                <DrawerTitle className=" font-normal text-md">Drafting</DrawerTitle>
-                                            </DrawerHeader>
-                                            <ScrollArea className="h-[100px] text-start rounded-md border p-2">
-                                                {quality.drafting}
-                                            </ScrollArea>
-                                            {
-                                                quality.lessing !== "" && (
-                                                    <>
-                                                        <DrawerHeader className="p-2">
-                                                            <DrawerTitle className=" font-normal text-md">Lessing</DrawerTitle>
-                                                        </DrawerHeader>
-                                                        <ScrollArea className="h-[100px] text-start rounded-md border p-2">
-                                                            {quality.lessing}
-                                                        </ScrollArea>
-                                                    </>
+                <TableBody>
 
-                                                )
-                                            }
-                                        </ScrollArea>
-                                    </DrawerContent>
-                                </Drawer>
-                            </TableCell>
-                            <TableCell>
-                                <DropdownMenu>
-                                    <DropdownMenuTrigger className=" border-none outline-none select-none"><MoreVertical /></DropdownMenuTrigger>
-                                    <DropdownMenuContent>
-                                        <DropdownMenuItem className="text-blue-500 hover:bg-blue-100" onClick={() => handleEdit(quality._id)}>Edit</DropdownMenuItem>
-                                        <DropdownMenuSeparator />
-                                        <DropdownMenuItem className="text-red-500 hover:bg-red-100" onClick={() => handleRemove(quality._id)}>Remove</DropdownMenuItem>
-                                    </DropdownMenuContent>
-                                </DropdownMenu>
-                            </TableCell>
-                        </TableRow>
-                    ))
-                }
+                    {
+                        quality.map((quality: any, index) => (
+                            <TableRow key={index}>
+                                <TableCell>
+                                    <Dialog>
+                                        <DialogTrigger>
+                                            <img src={quality.photo} className="h-10 w-10 object-cover" alt="photo" />
+                                        </DialogTrigger>
+                                        <DialogContent>
+                                            <img src={quality.photo} className="h-full w-full object-cover" alt="photo" />
+                                        </DialogContent>
+                                    </Dialog>
+                                </TableCell>
+                                <TableCell className="ps-4">
+                                    {quality.qualityName}
+                                </TableCell>
+                                <TableCell className="ps-4">
+                                    <Drawer>
+                                        <DrawerTrigger asChild>
+                                            <Button variant={"outline"} size={"sm"}>
+                                                Details
+                                            </Button>
+                                        </DrawerTrigger>
+                                        <DrawerContent className="mb-4">
+                                            <DrawerHeader>
+                                                <DrawerTitle>Costing</DrawerTitle>
+                                            </DrawerHeader>
+                                            <div className="px-3 pb-3">
+                                                <Table className=" text-center">
+                                                    <TableRow>
+                                                        <TableCell>Yarn Cost:</TableCell>
+                                                        <TableCell>{quality.yarnCost} Rs.</TableCell>
+                                                    </TableRow>
+                                                    <TableRow>
+                                                        <TableCell>Warping Cost:</TableCell>
+                                                        <TableCell>{quality.warpingCost} Rs.</TableCell>
+                                                    </TableRow>
+                                                    <TableRow>
+                                                        <TableCell>Extra Cost:</TableCell>
+                                                        <TableCell>{quality.extraCost} Rs.</TableCell>
+                                                    </TableRow>
+                                                    <TableRow>
+                                                        <TableCell>Job Cost:</TableCell>
+                                                        <TableCell>{quality.jobCost} Rs.</TableCell>
+                                                    </TableRow>
+                                                    <TableRow>
+                                                        <TableCell>Add Profit:</TableCell>
+                                                        <TableCell>{quality.addProfit} Rs.</TableCell>
+                                                    </TableRow>
+                                                    <TableRow>
+                                                        <TableCell>Selling Price:</TableCell>
+                                                        <TableCell>{quality.sellingPrice} Rs.</TableCell>
+                                                    </TableRow>
+                                                </Table>
+                                            </div>
+                                            <ScrollArea className="h-[400px] overflow-auto bg-zinc-100">
+                                                <DrawerHeader>
+                                                    <DrawerTitle>Construction</DrawerTitle>
+                                                </DrawerHeader>
+                                                <Table className=" text-center">
+                                                    <TableRow>
+                                                        <TableCell>Yarn:</TableCell>
+                                                        <TableCell>{quality.yarnType}</TableCell>
+                                                    </TableRow>
+                                                    <TableRow>
+                                                        <TableCell>Yarn Type:</TableCell>
+                                                        <TableCell>{quality.whichYarn}</TableCell>
+                                                    </TableRow>
+                                                </Table>
+                                                <DrawerHeader className="p-2">
+                                                    <DrawerTitle className=" font-normal text-md">Yarn Detail</DrawerTitle>
+                                                </DrawerHeader>
+                                                <ScrollArea className="h-[150px] text-start rounded-md border p-2">
+                                                    {quality.yarnDetail}
+                                                </ScrollArea>
+                                                <DrawerHeader className="p-2">
+                                                    <DrawerTitle className=" font-normal text-md">Yarn Pattern</DrawerTitle>
+                                                </DrawerHeader>
+                                                <ScrollArea className="h-[150px] text-start rounded-md border p-2">
+                                                    {quality.yarnPattern}
+                                                </ScrollArea>
+                                                <DrawerHeader className="p-2">
+                                                    <DrawerTitle className=" font-normal text-md">Reed</DrawerTitle>
+                                                </DrawerHeader>
+                                                <ScrollArea className="h-[100px] text-start rounded-md border p-2">
+                                                    {quality.reed}
+                                                </ScrollArea>
+                                                <DrawerHeader className="p-2">
+                                                    <DrawerTitle className=" font-normal text-md">Reed Pattern</DrawerTitle>
+                                                </DrawerHeader>
+                                                <ScrollArea className="h-[100px] text-start rounded-md border p-2">
+                                                    {quality.reedPattern}
+                                                </ScrollArea>
+                                                <DrawerHeader className="p-2">
+                                                    <DrawerTitle className=" font-normal text-md">Drafting</DrawerTitle>
+                                                </DrawerHeader>
+                                                <ScrollArea className="h-[100px] text-start rounded-md border p-2">
+                                                    {quality.drafting}
+                                                </ScrollArea>
+                                                {
+                                                    quality.lessing !== "" && (
+                                                        <>
+                                                            <DrawerHeader className="p-2">
+                                                                <DrawerTitle className=" font-normal text-md">Lessing</DrawerTitle>
+                                                            </DrawerHeader>
+                                                            <ScrollArea className="h-[100px] text-start rounded-md border p-2">
+                                                                {quality.lessing}
+                                                            </ScrollArea>
+                                                        </>
+
+                                                    )
+                                                }
+                                            </ScrollArea>
+                                        </DrawerContent>
+                                    </Drawer>
+                                </TableCell>
+                                <TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger className=" border-none outline-none select-none"><MoreVertical /></DropdownMenuTrigger>
+                                        <DropdownMenuContent>
+                                            <DropdownMenuItem className="text-blue-500 hover:bg-blue-100" onClick={() => handleEdit(quality._id)}>Edit</DropdownMenuItem>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem className="text-red-500 hover:bg-red-100" onClick={() => handleRemove(quality._id)}>Remove</DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
             </Table>
             <div className={` fixed items-center h-[100vh] w-[100vw] bg-white top-0 backdrop-blur-md flex ${!loading && " hidden"} my-3 justify-center w-full`}><Loader className=" animate-spin" /></div>
         </>
